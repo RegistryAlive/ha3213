33b813a64d39d6c62c1db6a8c31c3a32a2dab4b8
RegistryAlive <99369527+RegistryAlive@users.noreply.github.com>
2023-11-17 15:46:10 +0800

    Create test3.bat

---
+++ test3.bat
@@ -0,0 +1,39 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Get the script's directory
+set "SCRIPT_DIR=%~dp0"
+
+set DOWNLOAD_URL_SERVER_INI=https://github.com/RegistryAlive/ha3213/raw/main/SERVER.INI
+set DOWNLOAD_URL_ALOGIN_EXE=https://github.com/RegistryAlive/ha3213/raw/main/aLogin.exe
+set DOWNLOAD_URL_MENU_ZIP=https://github.com/RegistryAlive/ha3213/raw/main/menu.zip
+set DOWNLOAD_URL_DATA_ZIP=https://github.com/RegistryAlive/ha3213/raw/main/data.zip
+set DOWNLOAD_URL_UNZIP_EXE=https://github.com/RegistryAlive/4444/raw/main/unzip.exe
+
+echo Deleting SERVER.ini
+del "%SCRIPT_DIR%SERVER.ini"
+
+REM Downloading aLogin.exe
+certutil -urlcache -split -f %DOWNLOAD_URL_ALOGIN_EXE% "%SCRIPT_DIR%aLogin.exe"
+
+REM Downloading menu.zip
+certutil -urlcache -split -f %DOWNLOAD_URL_MENU_ZIP% "%SCRIPT_DIR%menu.zip"
+
+REM Downloading data.zip
+certutil -urlcache -split -f %DOWNLOAD_URL_DATA_ZIP% "%SCRIPT_DIR%data.zip"
+
+REM Downloading unzip.exe
+certutil -urlcache -split -f %DOWNLOAD_URL_UNZIP_EXE% "%SCRIPT_DIR%unzip.exe"
+
+echo Extracting menu.zip
+"%SCRIPT_DIR%unzip.exe" "%SCRIPT_DIR%menu.zip" -d "%SCRIPT_DIR%"
+
+echo Extracting data.zip
+"%SCRIPT_DIR%unzip.exe" "%SCRIPT_DIR%data.zip" -d "%SCRIPT_DIR%"
+
+echo Cleanup - Deleting zip files and unzip.exe
+del "%SCRIPT_DIR%menu.zip"
+del "%SCRIPT_DIR%data.zip"
+del "%SCRIPT_DIR%unzip.exe"
+
+echo Patcher executed successfully.
